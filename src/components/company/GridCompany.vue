92aeff4edf64b38f42e20dbebf372175347eeeec
Tiago Souza <tiago.souza@tron.com.br>
2021-12-10 10:37:04 -0300

    cutomizaçao grid companys

--- src/components/company/GridCompany.vue
+++ src/components/company/GridCompany.vue
@@ -1,13 +1,56 @@
 <template>
-  <DxDataGrid :data-source="companys" :show-borders="true">
-    <DxColumn data-field="id" caption="ID"/>
+  <DxDataGrid
+    :data-source="companys"
+    :show-borders="true"
+    :allow-column-reordering="true"
+    :allow-column-resizing="true"
+    :column-auto-width="true"
+    :row-alternation-enabled="true"
+  >
+    <DxExport
+      ref="dataGrid"
+      :enabled="true"
+      :allow-export-selected-data="false"
+    />
+    <dx-header-filter :visible="true" />
+    <DxFilterRow :visible="true" apply-filter="auto" />
+    <DxSearchPanel :visible="true" />
+    <DxColumn cell-template="btn-actions" width="70" caption="#" />
+    <!-- <DxColumn data-field="id" caption="ID" /> -->
     <DxColumn :width="250" data-field="name" caption="RAZÃO SOCIAL" />
-    <DxColumn data-field="cnpj" caption="CNPJ EMPRESA" />
+    <DxColumn data-field="cnpjFormat" caption="CNPJ EMPRESA" />
+    <DxColumn data-field="created_at" caption="DATA CRIAÇÃO" data-type="date" />
+    <ButtonGroup slot="btn-actions" slot-scope="data">
+      <Button
+        :to="{ name: 'editCompany', params: { id: data.key.id } }"
+        size="small"
+      >
+        <Icon type="md-create" />
+      </Button>
+      <Button @click="deleteCompany(data.key)" size="small">
+        <Icon type="md-close" />
+      </Button>
+    </ButtonGroup>
+    <DxPaging :page-size="10" />
+    <DxPager
+      :show-page-size-selector="true"
+      :allowed-page-sizes="[1, 10, 20, 50, 100]"
+      :show-info="true"
+    />
   </DxDataGrid>
 </template>
 <script>
 import "devextreme/data/odata/store";
-import { DxDataGrid, DxColumn } from "devextreme-vue/data-grid";
+import {
+  DxDataGrid,
+  DxColumn,
+  DxExport,
+  DxHeaderFilter,
+  DxFilterRow,
+  DxSearchPanel,
+  DxPager,
+  DxPaging,
+} from "devextreme-vue/data-grid";
 
 export default {
   props: {
@@ -16,6 +59,12 @@ export default {
   components: {
     DxDataGrid,
     DxColumn,
+    DxExport,
+    DxHeaderFilter,
+    DxFilterRow,
+    DxSearchPanel,
+    DxPager,
+    DxPaging,
   },
   data() {
     return {
@@ -25,6 +74,11 @@ export default {
   watch: {
     value(newValue) {
       this.companys = newValue || [];
+    },
+  },
+  methods: {
+    deleteCompany(data) {
+      console.log(data);
     },
   },
   created() {
